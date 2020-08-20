f818a3ef7a8c095e1c348d9395501b02feb46fa0
Jerome Boyer <boyerje@us.ibm.com>
2020-08-20 11:56:02 -0700

    start organizing code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Vaccine Order & Reefer Optimization
+
+The description of the problem of this component is described in [this article](https://ibm-cloud-architecture.github.io/vaccine-solution-main/design/voro/) in the main vaccine solution repository.
+
+This repository is the implementation of the microservice that exposes API to do the order and reefer assignment optimization. 
+
+The code is a python flask app (Appsody stack), deployable to OpenShift or Kubernetes cluster.
+
---
+++ server/api/controller.py
@@ -0,0 +1,44 @@
+
+from flask import Blueprint, request, abort
+import logging
+from flasgger import swag_from
+from flask_restful import Resource, Api
+from server.routes.prometheus import track_requests
+from server.domain.doaf_vaccine_order_optimizer import VaccineOrderOptimizer
+from flask import current_app as app
+"""
+ created a new instance of the Blueprint class and bound the Controller resource to it.
+"""
+
+control_blueprint = Blueprint("control", __name__)
+api = Api(control_blueprint)
+
+# The python-flask stack includes the prometheus metrics engine. You can ensure your endpoints
+# are included in these metrics by enclosing them in the @track_requests wrapper.
+
+class OrderShipmentController(Resource):
+    def get(self):
+        return {"status": "success", "message": "present "}
+    
+
+    # Need to support asynchronous HTTP Request, return 202 accepted while starting 
+    # the processing of generating events. The HTTP header needs to return a
+    # location to get the status of the simulator task    
+    @track_requests
+    @swag_from('controlapi.yml')
+    def post(self):
+        app.logger.info("post order received: ")
+        order = request.get_json(force=True)
+        app.logger.info(order)
+        # do some data validation
+        # prepare the data add the order to existing orders
+        optimizer = VaccineOrderOptimizer(start_date=date(2020, 7, 6), debug=False)
+        # call: optimizer.optimize(orders)
+
+        if not 'containerID' in order:
+            abort(400) 
+        return { "reason": "order optimization started"},202
+    
+
+
+api.add_resource(OrderShipmentController, "/voro/api/v1/")
