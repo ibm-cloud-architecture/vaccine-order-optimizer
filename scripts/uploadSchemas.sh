b5e5796357c7fbbb13e3ffc66667fa2bbef99080
Jerome Boyer <boyerje@us.ibm.com>
2021-03-26 17:41:18 -0700

    fix issue in schema path in uploadSchema locally

--- scripts/uploadSchemas.sh
+++ scripts/uploadSchemas.sh
@@ -3,16 +3,16 @@ scriptDir=$(dirname $0)
 
 curl -X POST -H "Content-type: application/json; artifactType=AVRO" \
    -H "X-Registry-ArtifactId: vaccine.reefers-value" \
-   --data ${scriptDir}/data/avro/schemas/reefer.avsc http://localhost:8080/api/artifacts
+   --data @${scriptDir}/../data/avro/schemas/reefer.avsc http://localhost:8080/api/artifacts
 
 curl -X POST -H "Content-type: application/json; artifactType=AVRO" \
    -H "X-Registry-ArtifactId: vaccine.inventory-value" \
-   --data ${scriptDir}/data/avro/schemas/inventory.avsc http://localhost:8080/api/artifacts
+   --data @${scriptDir}/../data/avro/schemas/inventory.avsc http://localhost:8080/api/artifacts
 
 curl -X POST -H "Content-type: application/json; artifactType=AVRO" \
    -H "X-Registry-ArtifactId: vaccine.transportation-value" \
-   --data ${scriptDir}/data/avro/schemas/transportation.avsc http://localhost:8080/api/artifacts
+   --data @${scriptDir}/../data/avro/schemas/transportation.avsc http://localhost:8080/api/artifacts
 
 curl -X POST -H "Content-type: application/json; artifactType=AVRO" \
    -H "X-Registry-ArtifactId: cloudEvent" \
-   --data ${scriptDir}/data/avro/schemas/cloudEvent.avsc http://localhost:8080/api/artifacts
+   --data @${scriptDir}/../data/avro/schemas/cloudEvent.avsc http://localhost:8080/api/artifacts
