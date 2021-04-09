0dbfb588ae7b4d5323266b51e810f4b6b4b115b0
Jesús Almaraz Hernández <jesusalmarazhernandez@jesuss-mbp.home>
2021-04-09 22:05:51 +0200

    reverting kafka bootstrap and adding it in the deployment only

--- scripts/setenv-tmpl.sh
+++ scripts/setenv-tmpl.sh
@@ -2,7 +2,7 @@
 ## Main properties ##
 #####################
 # Set these regardless of where your Event Streams on prem or on IBM Cloud or a local Kafka instance
-export KAFKA_BOOTSTRAP_SERVERS=""
+export KAFKA_BROKERS=""
 export REEFER_TOPIC="vaccine.reefers"
 export INVENTORY_TOPIC="vaccine.inventory"
 export TRANSPORTATION_TOPIC="vaccine.transportation"
